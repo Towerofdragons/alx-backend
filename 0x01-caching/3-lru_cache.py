0a99257be82cfdb64d5cfccbdd12b1303b2b4f8f
Mark <marknjoropge@gmail.com>
2024-10-30 21:44:57 +0000

    Tasks 2-4

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """
-    Implements a caching system using a Last-In-First-Out (FIFO) strategy.
+    Implements a caching system using a Least Recently Used (LRU) strategy.
 """
 
 
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """
         Adds an item to the cache data with
-        the specified key using LIFO replacement policy.
+        the specified key using LRU replacement policy.
     """
     def __init__(self):
         """
@@ -23,7 +23,7 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """
             Adds an item to the cache data
-            with the specified key using LIFO replacement policy.
+            with the LRU policy.
         """
         if not key or not item:
             return
@@ -31,12 +31,14 @@ class LIFOCache(BaseCaching):
         if key in self.cache_data:
             # Update the existing item; no need to adjust the order
             self.cache_data[key] = item
+            self.key_queue.remove(key)
+            self.key_queue.append(key)
             return
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            cleared_key = self.key_queue.pop(len(self.cache_data) - 1)
-            del self.cache_data[cleared_key]
-            print(f"DISCARD: {cleared_key}")
+            LRU_key = self.key_queue.pop(0)
+            del self.cache_data[LRU_key]
+            print(f"DISCARD: {LRU_key}")
 
         self.key_queue.append(key)
         self.cache_data[key] = item
@@ -45,5 +47,13 @@ class LIFOCache(BaseCaching):
         """
              Retrieves the item from the cache
              data associated with the specified key.
+             Re-adds the key to the queue once accessed
         """
+        # Attach use recency value
+        if self.cache_data.get(key, None):
+            self.key_queue.remove(key)
+            self.key_queue.append(key)
+        else:
+            return None
+
         return self.cache_data.get(key)
