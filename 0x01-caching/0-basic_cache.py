7af5e1de5726a3ee106088111ac12320427382b4
Mark <marknjoropge@gmail.com>
2024-10-30 18:24:10 +0000

    TFormat task 1 for pycodestyle

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,11 +8,15 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """
-        BasicCache class that inherits from BaseCaching.
-        Implements a basic caching system with no limit on the cache size.
+    BasicCache class that inherits from BaseCaching.
+    Implements a basic caching system with no limit on the cache size.
     """
 
     def put(self, key, item):
+        """
+         Adds an item to the cache data with the specified key
+        """
+    
         if not key or not item:
             return
         else:
@@ -20,4 +24,8 @@ class BasicCache(BaseCaching):
 
 
     def get(self, key):
+        """
+        Retrieves the item from the cache data associated with the specified key.
+
+        """
         return self.cache_data.get(key)
--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,45 +1,48 @@
 #!/usr/bin/env python3
-
-from base_caching import BaseCaching
-
 """
     Implements a caching system using a First-In-First-Out (FIFO) strategy.
 """
 
 
+from base_caching import BaseCaching
+
+
 class FIFOCache(BaseCaching):
     """
-        Adds an item to the cache data with the specified key using FIFO replacement policy.
+        Adds an item to the cache data with
+        the specified key using FIFO replacement policy.
     """
     def __init__(self):
         """
-             Calls the parent class (BaseCaching) initializer and sets up additional attributes.
+             Calls the parent class (BaseCaching) initializer
+             and sets up additional attributes.
         """
         super().__init__()
         self.key_queue = []
 
     def put(self, key, item):
         """
-            Adds an item to the cache data with the specified key using FIFO replacement policy.
+            Adds an item to the cache data
+            with the specified key using FIFO replacement policy.
         """
         if not key or not item:
             return
-        
+
         if key in self.cache_data:
             # Update the existing item; no need to adjust the order
             self.cache_data[key] = item
-      
+
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             cleared_key = self.key_queue.pop(0)
             del self.cache_data[cleared_key]
             print(f"DISCARD: {cleared_key}")
 
-          
         self.key_queue.append(key)
         self.cache_data[key] = item
 
     def get(self, key):
         """
-             Retrieves the item from the cache data associated with the specified key.
+             Retrieves the item from the cache
+             data associated with the specified key.
         """
         return self.cache_data.get(key)
