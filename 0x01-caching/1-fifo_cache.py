8324a992af95e468caa0f78f89db32313427e10e
Mark <marknjoropge@gmail.com>
2024-10-30 18:08:23 +0000

    Task 0 and 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,19 +3,25 @@
 from base_caching import BaseCaching
 
 """
-
+    Implements a caching system using a First-In-First-Out (FIFO) strategy.
 """
 
 
 class FIFOCache(BaseCaching):
     """
-
+        Adds an item to the cache data with the specified key using FIFO replacement policy.
     """
     def __init__(self):
+        """
+             Calls the parent class (BaseCaching) initializer and sets up additional attributes.
+        """
         super().__init__()
         self.key_queue = []
 
     def put(self, key, item):
+        """
+            Adds an item to the cache data with the specified key using FIFO replacement policy.
+        """
         if not key or not item:
             return
         
@@ -33,4 +39,7 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+             Retrieves the item from the cache data associated with the specified key.
+        """
         return self.cache_data.get(key)
