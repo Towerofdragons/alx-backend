78ff893109cf06ea92dbc7cd94ef4fe18f91d37c
Mark <marknjoropge@gmail.com>
2024-11-07 22:05:33 +0000

    Task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+A basic Flask app
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    """
+    Class perfroms babel configurations at instatiation.
+    """
+
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def home():
+    """
+    Route to home page.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()  # debug=True#)
