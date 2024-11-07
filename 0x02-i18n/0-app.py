b79b8e33b0334f16894bdb04361ad64cdecaeddc
Mark <marknjoropge@gmail.com>
2024-11-07 20:59:50 +0000

    Set up flask app for babel

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,32 @@
+#!usr/bin/env python3
+
+"""
+A basic Flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    def __init__(self, babel):
+        babel.default_locale = "en"
+        babel.default_timezone = "UTC"
+
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+config = Config(babel)
+
+
+
+
+@app.route('/')
+def home():
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
